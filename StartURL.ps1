7ec1eacf5e8d1c6d4a47dd38fac31e8a87c8e11d
ODLteam <dominique.duchatelet@vdabcampus.be>
2025-03-27 10:22:15 +0100

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,1 +1,1 @@
-Start-OSDCloud -ZTI -OSName 'Windows 11 24H2 ' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
+Start-OSDCloud -ZTI -OSName 'Windows 11 24H2' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
