16afb2eb892e57e2bacb5f22cd44eff4382f7b51
ODLteam <dominique.duchatelet@vdabcampus.be>
2025-03-31 13:20:22 +0100

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,4 +1,10 @@
 Start-OSDCloud -ZTI -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
 
+# Waiting for shutdonw
+For ($i = 30; $i -ge 0; $i--) {
+  Write-Host "Shutdown computer in $i seconds ..."
+  Start-Sleep -Seconds 1
+}
+
 # Shutdown computer after OSDCloud
 wpeutil shutdown
