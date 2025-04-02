00caf786508afe4106620428e6c33b1f05dc08f1
ODLteam <dominique.duchatelet@vdabcampus.be>
2025-04-02 10:13:30 +0100

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,6 +1,19 @@
+# Language
+Set-Culture -CultureInfo "nl-NL"
+Set-WinUILanguageOverride -Language "nl-NL"
+# Regional settings
+Set-WinHomeLocation -GeoID 227
+# Keyboard
+Set-WinDefaultInputMethodOverride "0413:00000413"
+# Waiting for 30 secs
+For ($i = 30; $i -ge 0; $i--) {
+  Write-Host "Shutdown computer in $i seconds ..."
+  Start-Sleep -Seconds 1
+}
+
 Start-OSDCloud -ZTI -OSName 'Windows 11 24H2 x64' -OSLanguage nl-nl -OSEdition Enterprise -OSActivation Volume
 
-# Waiting for shutdonw
+# Waiting for shutdown
 For ($i = 30; $i -ge 0; $i--) {
   Write-Host "Shutdown computer in $i seconds ..."
   Start-Sleep -Seconds 1
